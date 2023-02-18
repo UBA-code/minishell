bd6cac50111f8e0696ec4f1b7979fd0128ce4fe8
Yassine Bel Hachmi <ybel-hac@e2r11p9.1337.ma>
2023-02-18 11:47:56 +0100

    clean builtins folder

--- parser/builtins/env.c
+++ parser/builtins/env.c
@@ -1,47 +1,54 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   builtins_utils.c                                   :+:      :+:    :+:   */
+/*   env.c                                              :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: ybel-hac <ybel-hac@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/02/10 15:50:47 by ybel-hac          #+#    #+#             */
-/*   Updated: 2023/02/18 11:45:11 by ybel-hac         ###   ########.fr       */
+/*   Created: 2023/02/18 11:38:39 by ybel-hac          #+#    #+#             */
+/*   Updated: 2023/02/18 11:40:04 by ybel-hac         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../includes/minishell.h"
 
-void	modifie_variable(char *variable, char *new_value)
+int	env_search(char *variable)
 {
-	t_mini_env	*current;
+	t_mini_env *current;
 
 	current = g_global.env_head;
 	while (current)
 	{
 		if (ft_strcmp(current->name, variable))
-		{
-			free(current->value);
-			if (new_value)
-				current->value = ft_strdup(new_value);
-			else
-				current->value = 0;
-			return ;
-		}
-		current = current->next;
-	}
-}
-
-char	*get_variable_cmd(char *variable)
-{
-	t_mini_env	*current;
-
-	current = g_global.env_head;
-	while (current)
-	{
-		if (ft_strcmp(current->name, variable))
-			return (current->value);
+			return (1);
 		current = current->next;
 	}
 	return (0);
+}
+
+void	env_cmd(char c)
+{
+	t_mini_env	*current;
+
+	current = g_global.env_head;
+	if (c == 'x')
+	{
+		while (current)
+		{
+			if (current->value == 0)
+				printf("declare -x %s\n", current->name);
+			else
+				printf("declare -x %s=\"%s\"\n", current->name, current->value);
+			current = current->next;
+		}
+	}
+	else
+	{
+		while (current)
+		{
+			if (current->value)
+				printf("%s=%s\n", current->name, current->value);
+			current = current->next;
+		}
+	}
 }
