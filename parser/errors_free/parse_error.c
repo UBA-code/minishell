7f8f899a6980ed105919f849d03ae5849fc1a0ad
Yassine Bel Hachmi <ybel-hac@e2r11p9.1337.ma>
2023-02-24 13:34:23 +0100

    clean

---
+++ parser/errors_free/parse_error.c
@@ -0,0 +1,20 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   parse_error.c                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ybel-hac <ybel-hac@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/02/20 11:25:57 by ybel-hac          #+#    #+#             */
+/*   Updated: 2023/02/20 18:24:32 by ybel-hac         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../../includes/minishell.h"
+
+void	parse_error_free(t_lexer_node *lexer_head)
+{
+	lst_clear(&lexer_head);
+	free(lexer_head->cmd_struct.cmd);
+	ft_error("Error, Parse Error\n", 258);
+}
---
+++ parser/lexer_utils/lexer_utils.c
@@ -0,0 +1,37 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   lexer_utils.c                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ybel-hac <ybel-hac@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/02/19 16:31:03 by ybel-hac          #+#    #+#             */
+/*   Updated: 2023/02/19 16:32:25 by ybel-hac         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../../includes/minishell.h"
+
+void	init_lexer_node(t_lexer_node **head, char *line, char **env)
+{
+	int				i;
+	t_lexer			*lexer;
+	int				j;
+	t_lexer_node	*node;
+
+	lexer = malloc(sizeof(t_lexer) * (get_token_len(line) + 1));
+	i = 0;
+	j = 0;
+	while (line[i])
+	{
+		lexer[j].content = get_substring(line + i,
+				get_token_size(line + i, &i));
+		lexer[j].type = get_type(lexer[j].content);
+		j++;
+	}
+	lexer[j].content = 0;
+	node = lexer_create_node(head);
+	node->lexer = lexer;
+	node->lexer_size = get_token_len(line);
+	node->env = env;
+}
